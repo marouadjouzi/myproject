f2c86f178fc72b54c652b47cf09885d4ad674931
thenewinfo <ayadechir2003@gmail.com>
2023-11-03 10:41:58 +0100

    ss

--- acceuil.js
+++ acceuil.js
@@ -2,43 +2,42 @@ $(document).ready(function() {
   // Sélectionnez l'élément avec la classe "Platprincipale"
   $(".Platprincipale").hover(
     function() { // Fonction à exécuter lorsque survolé
-      $(this).css("background-image", "url('./flou.png')"); // Changez l'arrière-plan en bleu
+      $(this).css("background-image", "url('./flou.png')"); //Changer l'image de l'arrière plan 
     },
     function() { // Fonction à exécuter lorsque le survol se termine
-      $(this).css("background-image", "url('./second.png')"); // Rétablissez l'arrière-plan par défaut (vide)
+      $(this).css("background-image", "url('./second.png')"); // Rétablissez l'arrière-plan 
     }
   );
 });
 $(document).ready(function() {
-  // Sélectionnez l'élément avec la classe "Platprincipale"
   $(".salade").hover(
-    function() { // Fonction à exécuter lorsque survolé
-      $(this).css("background-image", "url('./flou2.png')"); // Changez l'arrière-plan en bleu
+    function() { 
+      $(this).css("background-image", "url('./flou2.png')"); 
     },
-    function() { // Fonction à exécuter lorsque le survol se termine
-      $(this).css("background-image", "url('./salade.png')"); // Rétablissez l'arrière-plan par défaut (vide)
+    function() {
+      $(this).css("background-image", "url('./salade.png')");
     }
   );
 });
 $(document).ready(function() {
-  // Sélectionnez l'élément avec la classe "Platprincipale"
+ 
   $(".Dessert").hover(
-    function() { // Fonction à exécuter lorsque survolé
-      $(this).css("background-image", "url('./flou4.png')"); // Changez l'arrière-plan en bleu
+    function() { 
+      $(this).css("background-image", "url('./flou4.png')");
     },
-    function() { // Fonction à exécuter lorsque le survol se termine
-      $(this).css("background-image", "url('./first.png')"); // Rétablissez l'arrière-plan par défaut (vide)
+    function() { 
+      $(this).css("background-image", "url('./first.png')"); 
     }
   );
 });
 $(document).ready(function() {
-  // Sélectionnez l'élément avec la classe "Platprincipale"
+ 
   $(".drinks").hover(
-    function() { // Fonction à exécuter lorsque survolé
-      $(this).css("background-image", "url('./flou3.png')"); // Changez l'arrière-plan en bleu
+    function() { 
+      $(this).css("background-image", "url('./flou3.png')"); 
     },
-    function() { // Fonction à exécuter lorsque le survol se termine
-      $(this).css("background-image", "url('./drinks.png')"); // Rétablissez l'arrière-plan par défaut (vide)
+    function() {
+      $(this).css("background-image", "url('./drinks.png')"); 
     }
   );
 });
