165894f5f78070b9444fae99144268ed00aba7be
thenewinfo <ayadechir2003@gmail.com>
2023-11-03 10:06:13 +0100

    new

--- acceuil.js
+++ acceuil.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  // Votre code jQuery va ici
-  $("button").click(function() {
-    alert("Bouton cliqué !");
-  });
+  // Sélectionnez l'élément avec la classe "element-survol"
+  $(".Platprincipale").hover(
+    function() { // Fonction à exécuter lorsque survolé
+      $(this).css("background-color", "#d1224e"); // Changez l'arrière-plan en bleu
+    },
+    function() { // Fonction à exécuter lorsque le survol se termine
+      $(this).css("background-image", "url('second.png')"); // Rétablissez l'arrière-plan par défaut (vide)
+    }
+  );
 });
